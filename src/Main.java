9319f46cdeff89f94b8d4a56c96f3e1cf43328c0
nikita <nikk555666@gmail.com>
2024-09-24 20:32:16 +0300

    asd

--- src/Main.java
+++ src/Main.java
@@ -84,4 +84,8 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Another one commit");
     }
+    {
+        System.out.println("Hello World!");
+        System.out.println("Another one commit");
+    }
 }
